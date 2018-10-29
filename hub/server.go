abff5f2970aa3d090af5e2d9f3f2fe432d6831f7
Daniel Randall <dgrandall@me.com>
2018-10-29 14:13:08 -0600

    init hub function

--- hub/server.go
+++ hub/server.go
@@ -9,8 +9,20 @@ import (
 	"github.com/byuoitav/central-event-system/hub/nexus"
 	"github.com/byuoitav/common"
 	"github.com/byuoitav/common/log"
+	"github.com/byuoitav/common/status"
 	"github.com/labstack/echo"
 )
+
+func init() {
+	// if this hub is in a room, create an interconnection with the rest of the hubs in the room
+	if len(os.Getenv("ROOM_SYSTEM")) > 0 {
+		addresses := GetHubAddresses()
+
+		for i := range addresses {
+			go hubconn.OpenConnection(addresses[i]+":7100", "/connect/hub", base.Hub, nexus.N)
+		}
+	}
+}
 
 func main() {
 	port := ":7100"
@@ -18,48 +30,52 @@ func main() {
 	nexus.StartNexus()
 
 	router := common.NewRouter()
+
+	router.GET("/status", Status)
 	router.GET("/connect/:type", func(context echo.Context) error {
 		t := context.Param("type")
 		switch t {
 		case base.Messenger, base.Repeater, base.Hub:
 			break
 		default:
-			return context.JSON(http.StatusBadRequest, "Invalid connection type")
+			return context.String(http.StatusBadRequest, "invalid connection type")
 		}
+
 		err := hubconn.CreateConnection(context.Response().Writer, context.Request(), t, nexus.N)
 		if err != nil {
 			return context.JSON(http.StatusInternalServerError, err.Error())
 		}
+
 		return nil
 	})
 
 	router.POST("/interconnect/:address", func(context echo.Context) error {
 		return CreateInterconnection(context, nexus.N)
 	})
-	router.GET("/mstatus", mstatus)
-	router.GET("/status", mstatus)
+
 	router.Start(port)
 }
 
-func mstatus(ctx echo.Context) error {
-	log.L.Debugf("MStatus request from %v", ctx.Request().RemoteAddr)
+// Status returns the status of the hub
+func Status(ctx echo.Context) error {
+	log.L.Debugf("Status request from %v", ctx.Request().RemoteAddr)
 
-	var s status.MStatus
+	var s status.Status
 	var err error
 
 	s.Bin = os.Args[0]
+	s.Info = make(map[string]interface{})
 
 	s.Version, err = status.GetMicroserviceVersion()
 	if err != nil {
-		s.Info = "failed to open version.txt"
+		s.Info["error"] = "failed to open version.txt"
 		s.StatusCode = status.Sick
 
 		return ctx.JSON(http.StatusInternalServerError, s)
 	}
-	s.Info = nexus.N.GetStatus()
 
+	s.Info["nexus"] = nexus.N.GetStatus()
 	s.StatusCode = status.Healthy
 
 	return ctx.JSON(http.StatusOK, s)
-
 }
