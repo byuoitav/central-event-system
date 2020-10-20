b1d6ba5f7f4a175717f62c6458d41e1ea43c134a
Daniel Randall <dgrandall@me.com>
2020-10-19 18:39:54 -0600

    deployed to new aws account

---
+++ terraform/repeater.tf
@@ -0,0 +1,25 @@
+module "event_repeater" {
+  source = "github.com/byuoitav/terraform//modules/kubernetes-deployment"
+
+  // required
+  name           = "event-repeater"
+  image          = "byuoitav/central-event-repeater"
+  image_version  = "latest"
+  container_port = 7101
+  repo_url       = "https://github.com/byuoitav/central-event-hub"
+
+  // optional
+  public_urls = ["event-repeater.av.byu.edu"]
+  private     = true
+  container_env = {
+    "DB_ADDRESS"       = data.aws_ssm_parameter.prd_db_addr.value,
+    "DB_USERNAME"      = data.aws_ssm_parameter.prd_db_username.value,
+    "DB_PASSWORD"      = data.aws_ssm_parameter.prd_db_password.value,
+    "HUB_ADDRESS"      = "http://event-hub"
+    "STOP_REPLICATIOn" = "true"
+    "SYSTEM_ID"        = "aws-repeater-system"
+    "VERSION"          = "0.1.0"
+  }
+  container_args = []
+  health_check   = false
+}
