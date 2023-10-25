d10e38c6e6ceb4525ef3e08d4615b53bf6bc27c5
Clinton Reeder <clinton_reeder@byu.edu>
2023-10-25 16:25:46 -0600

    Updated Terraform deployments

---
+++ terraform/dev/main.tf
@@ -0,0 +1,35 @@
+terraform {
+  backend "s3" {
+    bucket         = "terraform-state-storage-887007127029"
+    dynamodb_table = "terraform-state-lock-887007127029"
+    region         = "us-west-2"
+
+    // THIS MUST BE UNIQUE
+    key = "dev-central-event-system.tfstate"
+  }
+}
+
+provider "aws" {
+  region = "us-west-2"
+}
+
+data "aws_ssm_parameter" "eks_cluster_endpoint" {
+  name = "/eks/av-dev-cluster-endpoint"
+}
+
+provider "kubernetes" {
+  host        = data.aws_ssm_parameter.eks_cluster_endpoint.value
+  config_path = "~/.kube/config"
+}
+
+data "aws_ssm_parameter" "prd_db_addr" {
+  name = "/env/couch-new-address"
+}
+
+data "aws_ssm_parameter" "prd_db_username" {
+  name = "/env/couch-username"
+}
+
+data "aws_ssm_parameter" "prd_db_password" {
+  name = "/env/couch-password"
+}
---
+++ terraform/dev/repeater.tf
@@ -0,0 +1,27 @@
+module "event_repeater" {
+  //source = "github.com/byuoitav/terraform//modules/kubernetes-deployment"
+  source = "github.com/byuoitav/terraform-pod-deployment//modules/kubernetes-deployment"
+
+
+  // required
+  name           = "event-repeater-dev"
+  image          = "byuoitav/central-event-repeater"
+  image_version  = "latest"
+  container_port = 7101
+  repo_url       = "https://github.com/byuoitav/central-event-hub"
+
+  // optional
+  public_urls = ["event-repeater-dev.avdev.byu.edu"]
+  private     = true
+  container_env = {
+    "DB_ADDRESS"       = "https://${data.aws_ssm_parameter.prd_db_addr.value}",
+    "DB_USERNAME"      = data.aws_ssm_parameter.prd_db_username.value,
+    "DB_PASSWORD"      = data.aws_ssm_parameter.prd_db_password.value,
+    "HUB_ADDRESS"      = "ws://event-hub-dev"
+    "STOP_REPLICATION" = "true"
+    "SYSTEM_ID"        = "aws-repeater-system"
+    "VERSION"          = "0.1.0"
+  }
+  container_args = []
+  health_check   = false
+}
